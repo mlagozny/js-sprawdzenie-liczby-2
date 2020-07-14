afdccb203887ce68ca3837e0166b4b8b75348a1e
mlagozny <mateusz_lagozny@o2.pl>
2020-07-14 09:59:03 +0200

    sprawdzenie liczby

---
+++ main.js
@@ -0,0 +1,20 @@
+function numberIsEqual (num1, num2) {
+    if (num1 == 20) {
+        console.log('True');
+    } else {
+        console.log('');
+    }
+    if (num2 == 20) {
+        console.log('True');
+    } else {
+        console.log('');
+    }
+    if (num1 + num2 <= 20) {
+        console.log('True');
+    } else {
+        console.log('');
+    }
+}
+
+
+
