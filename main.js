ff780b0e5466e966a1f61ce0c01205e3437fd7f0
mlagozny <mateusz_lagozny@o2.pl>
2020-07-15 11:47:52 +0200

    poprawa zadania

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 function numberIsEqual (num1, num2) {
-    if (num1 == 20) {
-        console.log('True');
-    } else {
-        console.log('');
-    }
-    if (num2 == 20) {
-        console.log('True');
-    } else {
-        console.log('');
-    }
-    if (num1 + num2 <= 20) {
-        console.log('True');
-    } else {
-        console.log('');
-    }
+    if(num1 == 20 || num2 == 20 || (num1 + num2) <= 20 ) {
+        return true
+    };
 }
-
-
-
